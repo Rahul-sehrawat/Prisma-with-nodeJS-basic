9796f6c61e6e147e7a17c82985b0f2252292bd13
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-08-11 09:45:42 +0530

    get all users

--- index.ts
+++ index.ts
@@ -3,13 +3,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main(){
-    // Create user
-    const user = await prisma.user.create({
-        data:{
-            name: 'Rahul',
-            email: "rahul@gamil.com"
-        },
-    });
+    // ----- Create user ------
+    // const user = await prisma.user.create({
+    //     data:{
+    //         name: 'Rahul',
+    //         email: "rahul@gamil.com"
+    //     },
+    // });
+
+    //----- Get all users -----
+    const users = await prisma.user.findMany();
+
+ 
 
 
 
